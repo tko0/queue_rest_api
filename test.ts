6de23c2bc634a58edf7aa634651a6e4ac3b0e32c
Terrence Koo <terrencekoo@Terrences-MacBook-Air.local>
2024-04-08 10:54:59 -0400

    Updated redundant test

--- test.ts
+++ test.ts
@@ -23,17 +23,11 @@ console.log("Queue after adding multiple songs:", getQueue(), `, Expected: [
     { song_id: 4, song_name: "song_name", song_artist: "artist_name", position: 3 }
 ]`);
 
-// Try removing a song by providing its ID and position
-removeSongs([{ song_id: 2, song_name: 'song_name', song_artist: 'artist_name', position: 1 }]);
-console.log("Queue after removing a song by ID and position:", getQueue(), `, Expected: [
-    { song_id: 3, song_name: "song_name", song_artist: "artist_name", position: 1 },
-    { song_id: 4, song_name: "song_name", song_artist: "artist_name", position: 2 }
-]`);
+// Try removing multiple songs in the queue
+removeSongs([{ song_id: 2, song_name: 'song_name', song_artist: 'artist_name', position: 1 }, { song_id: 3, song_name: "song_name", song_artist: "artist_name", position: 2 }]);
+console.log("Queue after removing a song by ID and position:", getQueue(), `, Expected: [{ song_id: 4, song_name: "song_name", song_artist: "artist_name", position: 1 }]`);
 
 // Try removing a song that is not in the queue
 console.log("Trying to remove a song not in the queue:");
 console.log(removeSongs([{ song_id: 5, song_name: 'not_in_queue', song_artist: 'artist_name', position: 1 }]), ', Song not_in_queue (ID: 5, Position: 1) is not in the queue.');
-console.log("Queue remains:", getQueue(), `, Expected: [
-    { song_id: 3, song_name: "song_name", song_artist: "artist_name", position: 1 },
-    { song_id: 4, song_name: "song_name", song_artist: "artist_name", position: 2 }
-]`);
+console.log("Queue remains:", getQueue(), `, Expected: [{ song_id: 4, song_name: "song_name", song_artist: "artist_name", position: 1 }]`);
